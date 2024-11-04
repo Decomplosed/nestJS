58047a332dbb259ec223d886a791caa8c62634f5
Bart Frackiewicz <decomplosed@gmail.com>
2024-11-04 16:19:55 +0100

    Created a create method for adding a user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -48,4 +48,18 @@ export class UsersService {
 
     return user;
   }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const usersByHighestId = [...this.users].sort((a, b) => (b.id = a.id));
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
 }
